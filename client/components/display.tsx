7c957b4daa0b855a5c0f587031ad17be66a618ce
Robert Tutone <theonlylegitbobby@gmail.com>
2024-03-01 09:33:16 +1300

    Display File changed

--- client/components/display.tsx
+++ client/components/display.tsx
@@ -1,19 +1,12 @@
-interface Cocktail {
-    name: string;
-    ingredients: string[];
-    instructions: string;
-}
+import React from 'react';
 
-interface DisplayProps {
-    cocktail: Cocktail;
-}
-
-const Display: React.FC<DisplayProps> = ({ cocktail }) => {
+const Display = ({ cocktail }) => {
     return (
         <div className="display">
             <h3>{cocktail.name}</h3>
             <p>Ingredients: {cocktail.ingredients.join(', ')}</p>
             <p>Instructions: {cocktail.instructions}</p>
+            <p>{cocktail.isAlcoholic ? 'Alcoholic' : 'Non-Alcoholic'}</p>
         </div>
     );
 };
