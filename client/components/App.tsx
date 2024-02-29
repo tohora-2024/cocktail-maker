c19fdd7539c528399ad59cb0f3ec5c73ae0561fe
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-02-29 16:32:03 +1300

    Add a recipe, plus spirits ts file

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,9 @@
-
-
 function App() {
   return (
-    <>
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
     </div>
-    </>
   )
 }
 
---
+++ client/data/cocktails-recipes.ts
@@ -0,0 +1,16 @@
+interface Cocktail {
+  name: string
+  ingredients: string
+  instructions: string
+}
+
+const CocktailData = {
+  Cosmo: {
+    name: 'Cosmopolitan',
+    ingredients: 'Vodka, lime juice, triple sec, cranberry juice.',
+    instructions:
+      'Grab your favourite shaker. Combine vodka, lime juice, triple sec and cranberry juice with a bunch of ice. Shake. Serve with preferred garnish.',
+  },
+}
+
+export default data
