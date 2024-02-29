39ec1d13f4a416d488626d8ba07592923d5611ec
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-03-01 10:11:35 +1300

    Remove Spirits component

--- client/router.tsx
+++ client/router.tsx
@@ -5,14 +5,10 @@ import {
   Route,
 } from 'react-router-dom'
 import App from './components/App'
-import Spirits from './components/Spirits'
+// import Spirits from './components/Spirits'
 
 const router = createBrowserRouter(
-  createRoutesFromElements([
-    <Route path="/" element={<App />}>
-      <Route path="/spirits/:id" element={<Spirits />} />
-    </Route>,
-  ])
+  createRoutesFromElements([<Route path="/" element={<App />}></Route>])
 )
 
 export default router
