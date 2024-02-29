040d566281450e877a5c42bff1978668fc84e0a1
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-02-29 22:39:40 +1300

    Remove Home and Nav, incorproate into App and remove from router file

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import SpiritsData from '../data/spirits'
-import Nav from './Nav'
-import { Outlet } from 'react-router-dom'
+// import Nav from './Nav'
+import { Link, Outlet } from 'react-router-dom'
+import data from '../data/spirits.ts'
+
+const listSpirits = Object.keys(data).map((key) => ({
+  name: key,
+}))
 
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
-      <Nav />
-      <Outlet />
+      <ul>
+        {listSpirits.map((spirit) => (
+          <li key={spirit.name}>
+            <Link to="{`spirits/${spirit.name}`}">{spirit.name}</Link>
+          </li>
+        ))}
+      </ul>
+      {/* <Nav />
+      <Outlet /> */}
     </div>
   )
 }
--- client/router.tsx
+++ client/router.tsx
@@ -6,15 +6,11 @@ import {
 } from 'react-router-dom'
 import App from './components/App'
 import Spirits from './components/Spirits'
-import Home from './components/Home'
-import Recipe from './components/Recipe'
 
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
       <Route path="/spirits/:id" element={<Spirits />} />
-      <Route path="/spirits/:id/:cocktails" element={<Recipe />} />
     </Route>,
   ])
 )
