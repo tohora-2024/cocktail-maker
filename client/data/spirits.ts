b17e3d22f4fe3aaf2842fe60bd133c0f77979c5b
Joel Anderson <154105066+joel-anderson-24@users.noreply.github.com>
2024-02-29 16:41:01 +1300

    Add some gin recipes

---
+++ client/data/spirits.ts
@@ -0,0 +1,38 @@
+interface Cocktail {
+  name: string
+  ingredients: string
+  instructions: string
+}
+
+const CocktailData = {
+  Vodka: {
+    name: 'Vodka',
+    ingredients: [
+      { name: 'Cosmo' },
+      { name: 'Moscow Mule' },
+      { name: 'Vodka Tonic' },
+      { name: 'Vodka Sour' },
+    ],
+  },
+  Gin: {
+    name: 'Gin',
+    ingredients: [
+      { name: 'Gin and Tonic' },
+      { name: 'Flamingo' },
+      { name: 'Queens Cocktail' },
+      { name: 'Trinity' },
+    ],
+  },
+}
+
+export default CocktailData
+
+// Africa: {
+//   name: 'Africa',
+//   image: 'South-Africa.jpg',
+//   countries: [
+//     { code: 'AO', name: 'Angola' },
+//     { code: 'BF', name: 'Burkina Faso' },
+//     { code: 'BI', name: 'Burundi' },
+//     { code: 'BJ', name: 'Benin' },
+//     { code: 'BW', name: 'Botswana' },
