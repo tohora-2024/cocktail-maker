4bf09d41dea63516f6e623193eaf1cd80553e2b2
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-03-01 15:10:15 +1300

    Delete empty test file, remove pseudocode example

--- client/__tests__/ViewingRecipesForSpirit.test.tsx
+++ client/__tests__/ViewingRecipesForSpirit.test.tsx
@@ -14,36 +14,4 @@ describe('the spirit page for gin', () => {
   })
 })
 
-describe('')
-
-// describe('<Counter/>', () => {
-// it('should increment by 1 when the user clicks the button', async () => {
-//   // ARRANGE
-//   render(<Counter />)
-//   const user = userEvent.setup()
-
-//   // ACT
-//   const button = screen.getByRole('button', { name: 'Up!' })
-//   await user.click(button)
-
-//   // ASSERT
-//   const currentCount = screen.getByText(/The count is/)
-//   expect(currentCount).toHaveTextContent('The count is 1')
-// })
-
-// it('should increment by the amount in the input every time the user clicks the button', async () => {
-//   render(<Counter />)
-//   const user = userEvent.setup()
-
-//   const input = screen.getByLabelText(/Increment Size/)
-//   const button = screen.getByRole('button', { name: 'Up!' })
-//   const currentCount = screen.getByText(/The count is/)
-
-//   await user.clear(input)
-//   await user.type(input, '5')
-//   await user.click(button)
-//   await user.click(button)
-
-//   expect(currentCount).toHaveTextContent('The count is 10')
-// })
-// })
+// describe('')
